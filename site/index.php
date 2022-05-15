92d3774f696a9dc9bf40546e3d044b7a9546ad74
github_igorya <igorman.2016@gmail.com>
2022-05-15 23:27:12 +0400

    каркас сайта

---
+++ site/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset = "UTF-8">
+    <title>Лабораторные работы</title>
+    <link rel = "stylesheet" href = "/styles/style.css">
+    <!--<script type = "text/javascript" src = "/scripts/lab1script.js"></script>
+    <script type = "text/javascript" src = "/scripts/lab2script.js"></script>-->
+</head>
+<body>
+
+<div class="header">
+    <div class="h_container">
+        <div class="buttons">
+            <a class="button_link" href="#">Main Page</a>
+            <a class="button_link" href="#">My Page</a>
+            <a class="button_link" href="#">Gallery</a>
+            <a class="button_link" href="#">Play The Game</a>
+            <a class="button_link" href="#">My Works</a>
+            <a class="button_link" id="reg_log" href="#">Register | Login</a>
+        </div>
+    </div>
+</div>
+
+<div class="content">
+    <div class="c_container">
+<!--        <h1>Site of I.Ryabusha</h1>-->
+    </div>
+</div>
+<!---->
+<!--<div class="footer">-->
+<!---->
+<!--</div>-->
+
+
+<?php/*
+    <h1>Лабораторные работы Рябуши Игоря ИВТАСбд-12</h1>
+    <a href="labs.pages/lab1.php">Лабораторная работа №1</a><br>
+    <a href="labs.pages/lab2.php">Лабораторная работа №2</a><br>
+*/?>
+
+</body>
+</html>
